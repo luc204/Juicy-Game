5f91255c7d5084ce000d3d4d54fec539f6c4484b
luc204 <luchamillnz@gmail.com>
2025-04-07 21:36:50 +1200

    Full Game Upload

---
+++ Assets/Scripts/Object and world Scripts/ChestController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isOpen = false; // Track if the chest is open
+    private bool playerInRange = false;
+
+    private AudioSource audioSource;
+    public AudioClip openSound;
+    public AudioClip closeSound;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange) // When you press E
+        {
+            ToggleChest();
+            PlaySound();
+        }
+    }
+    void PlaySound()
+    {
+        if (audioSource != null)
+        {
+            if (isOpen && openSound != null)
+            {
+                audioSource.PlayOneShot(openSound);
+            }
+            else if (!isOpen && closeSound != null)
+            {
+                audioSource.PlayOneShot(closeSound);
+            }
+        }
+    }
+    void ToggleChest()
+    {
+        isOpen = !isOpen; // Flip the bool (if open, close; if closed, open)
+        animator.SetBool("IsOpen", isOpen);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Player scripts/PlayerController.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem dustParticles;
+    // this is the particle system for the dust particles
+
+    public float Speed = 2f;
+    public float JumpForce = 1f;
+    
+    public bool isGrounded;
+    public bool IsWalking ;
+    public bool isAttacking ;
+  
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public AudioSource audioSource;
+    public AudioClip WalkSound;
+    public AudioClip JumpSound;
+    public AudioClip LandSound;
+    public AudioClip AttackSound;
+
+    float footstepTimer = 0f;
+    public float footstepInterval = 0.4f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        gameObject.tag = "Player";
+        // this is so that im declaring the rigidbody and the tag of the player at the start
+    }
+
+    void Update()
+    {
+        if (IsWalking && isGrounded)
+        {
+            footstepTimer -= Time.deltaTime;
+            if (footstepTimer <= 0f)
+            {
+                PlayWalkSound();
+                footstepTimer = footstepInterval; // reset timer
+            }
+        }
+        else
+        {
+            footstepTimer = 0f; // reset if not walking
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+            animator.SetBool("IsWalking", true);
+            IsWalking = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+            animator.SetBool("IsWalking", true);
+            IsWalking = true;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            animator.SetBool("IsJumping", true);
+            PlayJumpSound();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+            PlayAttackSound();
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("IsWalking", false);
+            IsWalking = false;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("IsJumping", false);
+            dustParticles.Play();
+            PlayLandSound();
+
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    void PlayWalkSound()
+    {
+        if (audioSource != null && WalkSound != null)
+        {
+            audioSource.PlayOneShot(WalkSound);
+        }
+    }
+     void PlayJumpSound()
+    {
+        if (audioSource != null && JumpSound != null)
+        {
+            audioSource.PlayOneShot(JumpSound);
+        }
+    }
+      void PlayLandSound()
+    {
+        if (audioSource != null && LandSound != null)
+        {
+            audioSource.PlayOneShot(LandSound);
+        }
+    }
+      void PlayAttackSound()
+    {
+        if (audioSource != null && AttackSound != null)
+        {
+            audioSource.PlayOneShot(AttackSound);
+        }
+    }
+}
